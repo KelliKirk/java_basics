72445367b32c60b7bda226eb0ad0d1f2d2d87ddd
Kelli Kirk <kelli.kirk@voco.ee>
2025-09-05 11:09:27 +0300

    Exercise 16: Even or Odd?

---
+++ src/Week1/Ex16.java
@@ -0,0 +1,16 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class Ex16 {
+    public static void main(String[] args) {
+        Scanner reader=new Scanner(System.in);
+        System.out.println("Type a number:");
+        int number =  reader.nextInt();
+        if (number % 2 == 0){
+            System.out.println("The number is even");
+        } else {
+            System.out.println("The number is odd");
+        }
+    }
+}
