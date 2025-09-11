2389d083033410799e2719c99ef2326db2afaced
Kelli Kirk <kelli.kirk@voco.ee>
2025-09-11 12:29:04 +0300

    Exercise 88: Students

--- src/Week5/Main.java
+++ src/Week5/Main.java
@@ -1,18 +1,40 @@
 package Week5;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Apartment studioManhattan = new Apartment(1, 16, 5500);
-        Apartment twoRoomsBrooklyn = new Apartment(2, 38, 4200);
-        Apartment fourAndKitchenBronx = new Apartment(3, 78, 2500);
+        Student pekka = new Student("Pekka Mikkola", "013141590");
+        System.out.println("name: " + pekka.getName());
+        System.out.println("studentnumber: " + pekka.getStudentNumber());
+        System.out.println(pekka);
 
-        System.out.println( studioManhattan.larger(twoRoomsBrooklyn) );       // false
-        System.out.println( fourAndKitchenBronx.larger(twoRoomsBrooklyn) );   // true
+        ArrayList<Student> list = new ArrayList<Student>();
+        Scanner reader = new Scanner(System.in);
 
-        System.out.println( studioManhattan.priceDifference(twoRoomsBrooklyn) );        // 71600
-        System.out.println( fourAndKitchenBronx.priceDifference(twoRoomsBrooklyn) );    // 35400
+        while (true) {
+            System.out.print("name: ");
+            String name = reader.nextLine();
+            if(name.isEmpty()){
+                break;
+            }
+            System.out.print("studentnumber: ");
+            String studentNumber = reader.nextLine();
+            list.add(new Student(name, studentNumber));
+        }
 
-        System.out.println( studioManhattan.moreExpensiveThan(twoRoomsBrooklyn) );       // false
-        System.out.println( fourAndKitchenBronx.moreExpensiveThan(twoRoomsBrooklyn) );   // true
+        for(Student student : list){
+            System.out.println(student);
+        }
+
+        System.out.print("Give search name: ");
+        String term = reader.nextLine();
+        for(Student student : list){
+            if(student.getName().contains(term)){
+                System.out.println(student);
+            }
+        }
+
+        reader.close();
     }
 }
